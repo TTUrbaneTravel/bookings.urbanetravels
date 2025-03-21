2c7d946a8e902ad60f204efd63630769ffceab46
IT Urbane Travel <It@urbanetravels.com>
2025-03-21 07:23:43 +0000

    eight commit

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,24 +1,24 @@
-// import Breadcrumb from '../components/Breadcrumb.jsx';
-import ChartFour from '../components/ChartFour.jsx';
-import ChartOne from '../components/ChartOne.jsx';
-import ChartThree from '../components/ChartThree.jsx';
-import ChartTwo from '../components/ChartTwo.jsx';
+// // import Breadcrumb from '../components/Breadcrumb.jsx';
+// import ChartFour from '../components/ChartFour.jsx';
+// import ChartOne from '../components/ChartOne.jsx';
+// import ChartThree from '../components/ChartThree.jsx';
+// import ChartTwo from '../components/ChartTwo.jsx';
 
-const Chart = () => {
-  return (
-    <>
-      {/* <Breadcrumb pageName="Chart"  breadcrumbs={[{ name: 'Charts /', link: '#' }]} /> */}
+// const Chart = () => {
+//   return (
+//     <>
+//       {/* <Breadcrumb pageName="Chart"  breadcrumbs={[{ name: 'Charts /', link: '#' }]} /> */}
 
-      <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
-        <div className="col-span-12">
-          <ChartFour />
-        </div>
-        <ChartOne />
-        <ChartTwo />
-        <ChartThree />
-      </div>
-    </>
-  );
-};
+//       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
+//         <div className="col-span-12">
+//           <ChartFour />
+//         </div>
+//         <ChartOne />
+//         <ChartTwo />
+//         <ChartThree />
+//       </div>
+//     </>
+//   );
+// };
 
-export default Chart;
+// export default Chart;
